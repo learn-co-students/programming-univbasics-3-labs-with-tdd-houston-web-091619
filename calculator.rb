07d7d9185570a6a91ee66d3649e00f5f9fc75a9d
David Caldwell <david@avvitt.com>
2019-09-05 17:55:04 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,10 @@
-# Add your variables here
+first_number = 5.to_i
+second_number = 6.to_i
+sum = (first_number + second_number)
+puts sum
+difference = (first_number - second_number)
+puts difference
+product = (first_number * second_number)
+puts product
+quotient = (first_number/second_number)
+puts quotient
